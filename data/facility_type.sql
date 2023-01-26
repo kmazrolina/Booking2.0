3c8d04307a3fe455241c5288523ae90ce1a3832c
kmazrolina <121491288+kmazrolina@users.noreply.github.com>
2023-01-26 21:20:24 +0100

    Delete facility_type.sql

--- data/facility_type.sql
+++
@@ -1,6 +0,0 @@
-insert into facility_type (id, name, house_id) values (1, 'Indoor swimming pool', 34);
-insert into facility_type (id, name, house_id) values (2, 'Steam sauna', 56);
-insert into facility_type (id, name, house_id) values (3, 'Dry sauna', 70);
-insert into facility_type (id, name, house_id) values (4, 'Luggage storage', 9);
-insert into facility_type (id, name, house_id) values (5, 'Airport transfer', 78);
-insert into facility_type (id, name, house_id) values (6, 'Café', 32);
