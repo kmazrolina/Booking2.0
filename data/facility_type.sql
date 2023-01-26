dd773d8c332cbc1ca082723b6145f6d69952aad2
kmazrolina <121491288+kmazrolina@users.noreply.github.com>
2023-01-26 17:04:05 +0100

    Add files via upload

---
+++ data/facility_type.sql
@@ -0,0 +1,8 @@
+insert into facility_type (id, name) values (1, 'Hotel');
+insert into facility_type (id, name) values (2, 'Apartment');
+insert into facility_type (id, name) values (3, 'Hostel');
+insert into facility_type (id, name) values (4, 'Guesthouse');
+insert into facility_type (id, name) values (5, 'Bed and Breakfast');
+insert into facility_type (id, name) values (6, 'Holiday Home');
+insert into facility_type (id, name) values (7, 'Villa');
+insert into facility_type (id, name) values (8, 'Private room');
