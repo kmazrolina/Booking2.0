011b9a1cf78d60294c765e634581bf36297da98f
kmazrolina <121491288+kmazrolina@users.noreply.github.com>
2023-01-26 21:17:58 +0100

    Delete facility_type.sql

--- data/facility_type.sql
+++
@@ -1,6 +0,0 @@
-insert into facility_type (id, name) values (1, 'Internet');
-insert into facility_type (id, name) values (2, 'Food and drinks');
-insert into facility_type (id, name) values (3, 'Wellness and SPA');
-insert into facility_type (id, name) values (4, 'Cleaning services');
-insert into facility_type (id, name) values (5, 'Parking');
-insert into facility_type (id, name) values (6, 'Other');
