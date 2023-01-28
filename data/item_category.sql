ef3998e1bd6a276498da840a141dd1f677dda6fc
kmazrolina <121491288+kmazrolina@users.noreply.github.com>
2023-01-28 18:19:04 +0100

    Delete item_category.sql

--- data/item_category.sql
+++
@@ -1,6 +0,0 @@
-insert into item_category (id, name) values (1, 'Bathroom type');
-insert into item_category (id, name) values (2, 'Kitchen type');
-insert into item_category (id, name) values (3, 'Bathroom equipment');
-insert into item_category (id, name) values (4, 'Kitchen equipment');
-insert into item_category (id, name) values (5, 'Entertainment');
-insert into item_category (id, name) values (6, 'Furniture');
