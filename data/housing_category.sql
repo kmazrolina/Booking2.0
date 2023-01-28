c3427b538804dd3b85361f7833a2d52bce3286ac
kmazrolina <121491288+kmazrolina@users.noreply.github.com>
2023-01-28 18:18:57 +0100

    Delete housing_category.sql

--- data/housing_category.sql
+++
@@ -1,8 +0,0 @@
-insert into housing_category (id, name) values (1, 'Hotel');
-insert into housing_category (id, name) values (2, 'Apartment');
-insert into housing_category (id, name) values (3, 'Hostel');
-insert into housing_category (id, name) values (4, 'Guesthouse');
-insert into housing_category (id, name) values (5, 'Bed and Breakfast');
-insert into housing_category (id, name) values (6, 'Holiday Home');
-insert into housing_category (id, name) values (7, 'Villa');
-insert into housing_category (id, name) values (8, 'Private room');
