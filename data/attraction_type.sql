58ea68f7b1670a3156dccb1cf0a5bd11b4c7180f
kmazrolina <121491288+kmazrolina@users.noreply.github.com>
2023-01-28 18:24:47 +0100

    Add files via upload

---
+++ data/attraction_type.sql
@@ -0,0 +1,6 @@
+insert into attraction_type (id, name) values (1, 'view points');
+insert into attraction_type (id, name) values (2, 'view points');
+insert into attraction_type (id, name) values (3, 'historical monuments');
+insert into attraction_type (id, name) values (4, 'theme parks');
+insert into attraction_type (id, name) values (5, 'sport centers');
+insert into attraction_type (id, name) values (6, 'view points');
