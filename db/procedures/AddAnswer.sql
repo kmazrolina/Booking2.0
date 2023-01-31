5e14fc4170fd0120ee8285a0d9bfd2cf6e8bd82c
wikiheh <62021845+wikiheh@users.noreply.github.com>
2023-01-31 21:29:07 +0100

    Update AddAnswer.sql

--- db/procedures/AddAnswer.sql
+++ db/procedures/AddAnswer.sql
@@ -1,9 +1,22 @@
+GO
 CREATE OR ALTER PROCEDURE add_answer (@question_id INT,
 @author_id INT,
 @description VARCHAR(2000))
 AS
 BEGIN
+	IF @description = ''
+    BEGIN
+        RAISERROR('Nieprawidlowa odpowiedz.', 16, 1)
+        RETURN
+    END
+
+	IF NOT EXISTS (SELECT 1 FROM housing_question WHERE id = @question_id)
+    BEGIN
+        RAISERROR('Pytanie z id %d nie istnieje.', 16, 1, @question_id)
+        RETURN
+    END
 INSERT INTO question_answer(question_id,description)
 VALUES (@question_id, @description);
 END;
+GO
 --EXEC add_answer 38,100,'Yes'
