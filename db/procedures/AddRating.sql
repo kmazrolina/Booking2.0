83452a59ac3ba229d5121d6bd5877cf13373a36b
wikiheh <62021845+wikiheh@users.noreply.github.com>
2023-01-31 22:54:11 +0100

    Update AddRating.sql

--- db/procedures/AddRating.sql
+++ db/procedures/AddRating.sql
@@ -3,7 +3,11 @@ CREATE OR ALTER PROCEDURE add_rating (@housing_id INT, @author_id INT, @score IN
 AS
 BEGIN
   DECLARE @rating_id INT;
-
+  IF @score > 10 OR @score < 1 
+  BEGIN
+  RAISERROR ('Nieprawidlowa ocena. Wybierz liczbe od 1 do 10', 16, 1);
+RETURN;
+END
   INSERT INTO rating_data (score, title, description)
   VALUES (@score, @title, @description);
 
