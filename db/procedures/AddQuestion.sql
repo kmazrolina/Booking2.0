2434f4c2cde886441d2c6a99b0c00779b9f691c2
wikiheh <62021845+wikiheh@users.noreply.github.com>
2023-01-31 21:38:26 +0100

    Update AddQuestion.sql

--- db/procedures/AddQuestion.sql
+++ db/procedures/AddQuestion.sql
@@ -2,12 +2,21 @@ GO
 CREATE OR ALTER PROCEDURE add_question (@housing_name NVARCHAR(100), @author_id INT, @description VARCHAR(2000))
 AS
 BEGIN
-DECLARE @housing_id INT;
-  
+  DECLARE @housing_id INT;
+  IF @description = ''
+    BEGIN
+        RAISERROR('Nieprawidlowe pytanie.', 16, 1)
+        RETURN
+    END
+
   SELECT @housing_id = id
   FROM housing
   WHERE name = @housing_name;
-
+  IF @housing_id IS NULL
+    BEGIN
+        RAISERROR('Podany dom nie istnieje.', 16, 1, @housing_name)
+        RETURN
+    END
   INSERT INTO housing_question (housing_id, author_id, description)
   VALUES (@housing_id, @author_id, @description);
 END;
