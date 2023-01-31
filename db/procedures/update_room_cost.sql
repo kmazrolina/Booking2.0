748d22312422d1192a753a9fb353de7eea93c183
wikiheh <62021845+wikiheh@users.noreply.github.com>
2023-01-31 21:17:24 +0100

    Update update_room_cost.sql

--- db/procedures/update_room_cost.sql
+++ db/procedures/update_room_cost.sql
@@ -1,7 +1,13 @@
+EXEC DropObject 'update_room_cost'
 GO
-CREATE  PROCEDURE update_room_cost (@id INT, @new_cost MONEY)
+CREATE PROCEDURE update_room_cost (@id INT, @new_cost MONEY)
 AS
-BEGIN
+BEGIN 
+IF @new_cost < 0
+    BEGIN
+        RAISERROR('Podana cena jest nieprawidlowa.', 16, 1)
+        RETURN
+    END
     UPDATE room
     SET cost_per_night = @new_cost
     WHERE id = @id;
