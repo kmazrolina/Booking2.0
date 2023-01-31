ad0d4101f428cda68ffc6337822cbf8c841f6f69
batiuszkamaroz <dawid.prajzner01@gmail.com>
2023-01-31 20:10:50 +0100

    fix

--- db/data/attraction_type.sql
+++
@@ -1,6 +0,0 @@
-insert into attraction_type (id, name) values (1, 'view points');
-insert into attraction_type (id, name) values (2, 'view points');
-insert into attraction_type (id, name) values (3, 'historical monuments');
-insert into attraction_type (id, name) values (4, 'theme parks');
-insert into attraction_type (id, name) values (5, 'sport centers');
-insert into attraction_type (id, name) values (6, 'view points');
