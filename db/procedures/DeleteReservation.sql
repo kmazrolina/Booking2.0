676ecf0525aa9ed9cc89c595266f69a63b4a66d4
kmazrolina <121491288+kmazrolina@users.noreply.github.com>
2023-01-31 16:01:21 +0100

    Add files via upload

---
+++ db/procedures/DeleteReservation.sql
@@ -0,0 +1,20 @@
+EXEC DropObject 'DeleteReservation'
+
+GO
+CREATE PROCEDURE DeleteReservation(@reservation_id int)
+AS
+BEGIN
+SET IMPLICIT_TRANSACTIONS OFF
+
+BEGIN TRANSACTION
+
+	--tutaj uruchomiony zostanie wyzwalacz aktualizujacy widoki zawierajace rezerwacje
+	DELETE FROM reservation
+	WHERE id = @reservation_id
+	
+	DELETE FROM reservation_details
+	WHERE reservation_id = @reservation_id
+
+COMMIT
+END
+GO
