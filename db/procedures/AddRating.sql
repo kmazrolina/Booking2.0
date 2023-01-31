30ca9ec77be1035b6f0e6692f2da3cbeb456bfb0
wikiheh <62021845+wikiheh@users.noreply.github.com>
2023-02-01 00:49:43 +0100

    Update AddRating.sql

--- db/procedures/AddRating.sql
+++ db/procedures/AddRating.sql
@@ -1,5 +1,6 @@
 GO
-CREATE OR ALTER PROCEDURE add_rating (@housing_id INT, @author_id INT, @score INT, @title VARCHAR(100), @description VARCHAR(2000))
+CREATE OR ALTER PROCEDURE add_rating (@housing_id INT, @object_type varchar(50),
+@author_id INT, @score INT, @title VARCHAR(100), @description VARCHAR(2000))
 AS
 BEGIN
   DECLARE @rating_id INT;
@@ -8,8 +9,32 @@ BEGIN
   RAISERROR ('Nieprawidlowa ocena. Wybierz liczbe od 1 do 10', 16, 1);
 RETURN;
 END
-  INSERT INTO rating_data (score, title, description)
-  VALUES (@score, @title, @description);
+
+IF @object_type = 'housing'
+BEGIN
+  INSERT INTO housing_rating (housing_id, author_id, rating_data_id)
+  VALUES (@housing_id, @author_id, @rating_id);
+END
+
+ IF @object_type = 'host'
+BEGIN
+ INSERT INTO host_rating(host_id, author_id, rating_data_id)
+ VALUES (@housing_id, @author_id, @rating_id);
+
+END
+
+IF @object_type = 'guest'
+BEGIN
+INSERT INTO guest_rating(guest_id, author_id, rating_data_id)
+ VALUES (@housing_id, @author_id, @rating_id);
+
+END
+
+IF @object_type = 'attraction'
+BEGIN
+INSERT INTO attraction_rating(attraction_id, author_id, rating_data_id)
+ VALUES (@housing_id, @author_id, @rating_id);
+END
 
   INSERT INTO housing_rating (housing_id, author_id, rating_data_id)
   VALUES (@housing_id, @author_id, @rating_id);
