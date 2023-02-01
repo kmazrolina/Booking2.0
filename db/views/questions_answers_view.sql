9ac7f4b3ced89feb81c517eabc2b4c8cf505a466
wikiheh <62021845+wikiheh@users.noreply.github.com>
2023-02-01 14:32:58 +0100

    Update questions_answers_view.sql

--- db/views/questions_answers_view.sql
+++ db/views/questions_answers_view.sql
@@ -1,8 +1,8 @@
 --- widok q&a
 DROP VIEW questions_answers_view
 GO
-
 CREATE VIEW  questions_answers_view AS
 SELECT  q.author_id AS question_author, q.description AS question, a.description AS answer
 FROM housing_question q
 JOIN question_answer a ON q.id=a.question_id;
+
