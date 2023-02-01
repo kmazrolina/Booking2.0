f72bb7bc8ef3681b1dd28e23807aee43c2dee529
wikiheh <62021845+wikiheh@users.noreply.github.com>
2023-02-01 16:02:21 +0100

    Update AddUser.sql

--- db/procedures/AddUser.sql
+++ db/procedures/AddUser.sql
@@ -1,8 +1,29 @@
-GO
-CREATE OR ALTER PROCEDURE add_user (@first_name varchar(100), @last_name varchar(100), @email varchar(100), @phone varchar(20), @gender varchar(20))
+CREATE OR ALTER PROCEDURE add_user (@first_name VARCHAR(100), @last_name VARCHAR(100), @email VARCHAR(100), @phone VARCHAR(20), @gender VARCHAR(20))
 AS
 BEGIN
-  INSERT INTO user_data (first_name, last_name, email, phone, gender)
-  VALUES (@first_name, @last_name, @email, @phone, @gender)
+  SET NOCOUNT ON;
+
+  DECLARE @error_message VARCHAR(MAX);
+
+  BEGIN TRY
+    IF (@first_name IS NULL OR @first_name = '') OR (@last_name IS NULL OR @last_name = '')
+    BEGIN
+      SET @error_message = 'nieprawidlowe imie lub nazwisko';
+      RAISERROR(@error_message, 16, 1);
+    END
+    ELSE IF EXISTS (SELECT 1 FROM user_data WHERE email = @email)
+    BEGIN
+      SET @error_message = 'podany email juz istnieje';
+      RAISERROR(@error_message, 16, 1);
+    END
+    ELSE
+    BEGIN
+      INSERT INTO user_data (first_name, last_name, email, phone, gender)
+      VALUES (@first_name, @last_name, @email, @phone, @gender);
+    END
+  END TRY
+  BEGIN CATCH
+    PRINT 'Wystapil nastepujacy blad: ' + ERROR_MESSAGE();
+  END CATCH
 END
 GO
