84ed3752db305a63e4e4b18868a1bb2b4d11ab0e
wikiheh <62021845+wikiheh@users.noreply.github.com>
2023-02-01 15:19:18 +0100

    Update and rename tr_update_houses_per_city_per_price to tr_update_houses_per_city_per_price.sql

--- db/triggers/tr_update_houses_per_city_per_price.sql
+++ db/triggers/tr_update_houses_per_city_per_price.sql
@@ -5,11 +5,9 @@ AFTER INSERT
 AS
 BEGIN
 EXEC DropObject 'houses_per_city_per_price'
-GO
-CREATE VIEW houses_per_city_per_price
+EXEC('CREATE VIEW houses_per_city_per_price
 AS
 SELECT C.city_name, C.housing_name, MIN(R.cost_per_night) [prices from] FROM houses_per_city C JOIN room R ON C.housing_id = R.housing_id
-GROUP BY C.housing_name
-GO
+GROUP BY C.housing_name')
 END
 GO
